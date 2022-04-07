f333fd4a89832de8176f554cd58cc5c8df31b2ae
Reyhan Türkkal <94196332+reyhan8trkkl@users.noreply.github.com>
2022-04-07 20:14:27 +0300

    Add files via upload

---
+++ server-html/server-html.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const http = require('http')
+
+const myServer = http.createServer((req,res)=>{
+    console.log('request')
+    res.writeHead(200,{'content-type':'text/html;charset=utf-8'})
+    fs.readFile('server.html',(err,data)=>{
+        if(err){
+            throw err;
+        }
+        response.end(data)
+    })
+})
+
+myServer.listen(3000)
